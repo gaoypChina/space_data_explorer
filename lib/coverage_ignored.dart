ca4af7d9c5d2b6427f42efba57207feb4dbbeb5c
Hrishikesh Kadam <hrkadam.92@gmail.com>
2024-04-04 03:24:14 +0530

    Remove unused method printMediaQuery()

--- lib/coverage_ignored.dart
+++ lib/coverage_ignored.dart
@@ -1,13 +1,10 @@
 // coverage:ignore-file
 
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 
-import 'package:hrk_logging/hrk_logging.dart';
 import 'package:recase/recase.dart';
 
 import 'constants/constants.dart';
-import 'globals.dart';
 
 final String appNameKebabCase = Constants.appName.paramCase;
 
@@ -21,11 +18,4 @@ bool isKeyboardVisibilitySupported() {
       _ => false
     };
   }
-}
-
-// LABEL: eligible-hrk_flutter_batteries
-void printMediaQuery(BuildContext context) {
-  final mediaQuery = MediaQuery.of(context);
-  logger.debug('mediaQuery.size = ${mediaQuery.size}');
-  logger.debug('mediaQuery.devicePixelRatio = ${mediaQuery.devicePixelRatio}');
 }
