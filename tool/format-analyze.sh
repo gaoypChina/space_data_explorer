6701c21aaa846c8e8a4af57f586070c0706718d0
Hrishikesh Kadam <hrkadam.92@gmail.com>
2024-04-06 12:23:28 +0530

    Add required fix for 3.22.0-0.1.pre

--- tool/format-analyze.sh
+++ tool/format-analyze.sh
@@ -12,4 +12,8 @@ dart format --output none --set-exit-if-changed .
 
 dart run import_sorter:main --exit-if-changed
 
+# TODO(hrishikesh-kadam): Required after 3.22.0-0.1.pre
+# Keep checking if really needed
+flutter pub get
+
 flutter analyze
